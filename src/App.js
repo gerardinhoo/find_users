545594568816b7723b4a5ff21292ce5122e736df
Gerard Eklu <gerard_eklu@homedepot.com>
2019-09-26 09:03:33 -0400

    Implemented hooks to this hooks branch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
@@ -8,55 +8,41 @@ import About from "./components/pages/About";
 import axios from "axios";
 
 const App = () => {
-
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false)
-
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    async(() => {
-      setLoading(true)
+    async function fetchData() {
+      setLoading(true);
       const res = await axios.get("https://randomuser.me/api/?results=12");
-      setUsers(res.data.results)
-      setLoading(false)
+      setUsers(res.data.results);
+      setLoading(false);
       console.log(res.data);
-    })
-  })
+    }
+    fetchData();
+  }, []);
 
-
-  // async componentDidMount() {
-  //   setLoading(true)
-  //   const res = await axios.get("https://randomuser.me/api/?results=12");
-  //   setUsers(res.data.results)
-  //   setLoading(false)
-  //   console.log(res.data);
-  // }
-
-    return (
-      <Router>
-        <div>
-          <Navbar title="Random Users Finder" icon="fas fa-users" />
-          <div className="container">
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={props => (
-                  <Fragment>
-                    <Users
-                      loading={loading}
-                      users={users}
-                    />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
+  return (
+    <Router>
+      <div>
+        <Navbar title="Random Users Finder" icon="fas fa-users" />
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Fragment>
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path="/about" component={About} />
+          </Switch>
         </div>
-      </Router>
-    );
-  }
-
+      </div>
+    </Router>
+  );
+};
 
 export default App;
