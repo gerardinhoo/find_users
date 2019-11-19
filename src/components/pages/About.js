dfbc439bc55d3775b9decc2aa4d49550288dd1e0
Gerard Eklu <gerard_eklu@homedepot.com>
2019-11-18 22:50:30 -0500

    Changed paragraph

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <p>A simple about generating random users through the Random User API.</p>
+      <p>A simple app generating random users through the Random User API.</p>
       <p>Version 1.0.0</p>
     </div>
   );
